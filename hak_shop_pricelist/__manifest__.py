6ec9de54ca015a360f4c6815885a45806e5d9e02
Hunain AK <55621092+HunainAhmedKhan@users.noreply.github.com>
2023-07-22 16:25:17 +0500

    Add files via upload

---
+++ hak_shop_pricelist/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Hide Shop Pricelist",
+
+    'summary': """
+        Hide Shop Pricelist for public user
+        
+       """,
+
+    'description': """
+        Hide Shop Pricelist for public user
+        Pricelist dropdown will show for internal and portal user.
+        Only logged in users can see the pricelist selector
+    """,
+
+     'author': "HAK Technologies",
+    'website': "http://www.HAKTechnologies.com",
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'website_sale'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+
+}
---
+++ hak_shop_pricelist/models/models.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class ProductProductInh(models.Model):
+    _inherit = 'product.product'
+
+    def get_product_multiline_description_sale(self):
+        """ Compute a multiline description of this product, in the context of sales
+                (do not use for purchases or other display reasons that don't intend to use "description_sale").
+            It will often be used as the default description of a sale order line referencing this product.
+        """
+        name = self.display_name
+
+        return name
